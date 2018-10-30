30e64b5f87d98fbcf83988e1f15436b416dfe6f6
Ayush <singhayush.2711@gmail.com>
2018-10-30 21:25:59 +0530

    Update MediaWidget.java

--- app/src/main/java/com/example/ayush/musica/widget/MediaWidget.java
+++ app/src/main/java/com/example/ayush/musica/widget/MediaWidget.java
@@ -183,39 +183,6 @@ public class MediaWidget extends AppWidgetProvider {
         pendingIntent = PendingIntent.getService(context, 0, intent, 0);
         views.setOnClickPendingIntent(R.id.widget_previous_btn, pendingIntent);
 
-    }
-
-    public   Bitmap decodeSampledBitmapFromResource(
-            String pathName) {
-        int reqWidth,reqHeight;
-       // reqWidth = ;
-       // reqWidth = (reqWidth/5)*2;
-        //reqHeight = reqWidth;
-        final BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inJustDecodeBounds = true;
-//  BitmapFactory.decodeStream(is, null, options);
-        BitmapFactory.decodeFile(pathName, options);
-// Calculate inSampleSize
-      //  options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-// Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false;
-        return BitmapFactory.decodeFile(pathName, options);
-    }
-
-    public   int calculateInSampleSize(BitmapFactory.Options options,
-                                       int reqWidth, int reqHeight) {
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-            if (width > height) {
-                inSampleSize = Math.round((float) height / (float) reqHeight);
-            } else {
-                inSampleSize = Math.round((float) width / (float) reqWidth);
-            }
-        }
-        return inSampleSize;
-    }
+    }   
 }
 
